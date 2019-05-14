780d816785988d2777d81bcf56a5e00c6298e7fa
Terrence Nixa <tnixa@us.ibm.com>
2019-05-14 15:02:00 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-## Test project for Watson Visual Recognition on IBM Cloud using Spring Boot Starter and CFEnv
-Project to test a Watson Visual Recognition service instance on IBM Cloud using Spring Boot Starter: https://github.com/watson-developer-cloud/spring-boot-starter. A sample jpg file will be analyzed.
-
-Uses the CFEnv processor to set the properties from the service entry in VCAP_SERVICES.
-- https://github.com/pivotal-cf/java-cfenv
+## Test project for MQ on IBM Cloud
+Project to test an MQ service instance on IBM Cloud. A sample will be sent and received. 
 
 ## Setup
 1. You'll need ibm cloud CLI from https://cloud.ibm.com/docs/cli/reference/ibmcloud?topic=cloud-cli-install-ibmcloud-cli#install_use and bx cf installed and configured to talk to the appropriate cf org/space etc.
--- src/main/java/org/terrence/testapp/rest/TestRestController.java
+++ src/main/java/org/terrence/testapp/rest/TestRestController.java
@@ -1,59 +1,38 @@
 package org.terrence.testapp.rest;
 
-import java.io.InputStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-
-import com.ibm.watson.visual_recognition.v3.VisualRecognition;
-import com.ibm.watson.visual_recognition.v3.model.ClassifiedImages;
-import com.ibm.watson.visual_recognition.v3.model.ClassifyOptions;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.jms.JmsException;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableJms
 public class TestRestController {
 
   @Autowired
-  protected VisualRecognition visualRecognition;
+  private JmsTemplate jmsTemplate;
 
-  // Create the input stream from the jpg file
-
-  public static InputStream createInputStream() throws Exception {
-    InputStream inputStream = TestRestController.class.getResourceAsStream("/640px-IBM_VGA_90X8941_on_PS55.jpg");
-    return inputStream;
-  }
-
-  // Test the Visual Recognizer by analyzing a test image
-
-  @RequestMapping(value = "/test", produces = "text/plain")
-  public String runTest() {
-    StringWriter sw = new StringWriter();
-    PrintWriter pw = new PrintWriter(sw);
-
+  @GetMapping("send")
+  String send() {
     try {
-      pw.println("Beginning test...");
-
-      ClassifyOptions classifyOptions = new ClassifyOptions.Builder().imagesFile(createInputStream())
-          .imagesFilename("640px-IBM_VGA_90X8941_on_PS55.jpg").build();
-
-      ClassifiedImages result = visualRecognition.classify(classifyOptions).execute().getResult();
-      System.out.println(result);
-
-      // check to see if query exists in the results
-      String expectedKeyword = "computer circuit";
-      if (result.toString().toLowerCase().contains(expectedKeyword.toLowerCase())) {
-        pw.println("PASS: Visual Recognizer results contain expected keyword: " + expectedKeyword);
-      } else {
-        pw.println("FAIL: Visual Recognizer results do not contain expected keyword: " + expectedKeyword);
-      }
-
-    } catch (Exception e) {
-      pw.println("FAIL: Unexpected error during test.");
-      e.printStackTrace();
+      jmsTemplate.convertAndSend("DEV.QUEUE.1", "Hello World!");
+      return "OK";
+    } catch (JmsException ex) {
+      ex.printStackTrace();
+      return "FAIL";
     }
-    pw.flush();
-    return sw.toString();
   }
+
+  @GetMapping("recv")
+  String recv() {
+    try {
+      return jmsTemplate.receiveAndConvert("DEV.QUEUE.1").toString();
+    } catch (JmsException ex) {
+      ex.printStackTrace();
+      return "FAIL";
+    }
+  }
+
 }
